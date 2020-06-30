ebb0c8ae927608f4e7ad6b5e9d998d3fe4771799
SavostinIllia <savostin.illia@gmail.com>
2020-07-01 00:18:22 +0200

    Adding validation for answers. If getting positive answer, waiting for next question, else do nothing

--- src/components/AnswerList/AnswerItem/AnswerItem.js
+++ src/components/AnswerList/AnswerItem/AnswerItem.js
@@ -2,14 +2,14 @@ import React from 'react'
 import classes from './AnswerItem.module.css'
 
 const AnswerItem = props => {
-  console.log('Item props:' , props)
+  // console.log('Item props:' , props)
 
   const cls=[classes.AnswerItem]
 
   if(props.state){
     cls.push(classes[props.state])
   }
-  console.log(classes)
+  
   return(
       <li
         id={props.answer.id}
