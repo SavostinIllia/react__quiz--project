26e12d7b4701e257dae855dfe7f7705999ce1446
SavostinIllia <savostin.illia@gmail.com>
2020-07-01 10:11:30 +0200

    after resolve merge conflict

--- src/components/ActiveQuiz/AnswerList/AnswerItem/AnswerItem.js
+++ src/components/ActiveQuiz/AnswerList/AnswerItem/AnswerItem.js
@@ -2,20 +2,12 @@ import React from 'react'
 import classes from './AnswerItem.module.css'
 
 const AnswerItem = props => {
-<<<<<<< HEAD:src/components/AnswerList/AnswerItem/AnswerItem.js
-=======
-  // console.log('Item props:' , props)
->>>>>>> develop:src/components/ActiveQuiz/AnswerList/AnswerItem/AnswerItem.js
 
   const cls=[classes.AnswerItem]
 
   if(props.state){
     cls.push(classes[props.state])
   }
-<<<<<<< HEAD:src/components/AnswerList/AnswerItem/AnswerItem.js
-=======
-  
->>>>>>> develop:src/components/ActiveQuiz/AnswerList/AnswerItem/AnswerItem.js
   return(
       <li
         id={props.answer.id}
