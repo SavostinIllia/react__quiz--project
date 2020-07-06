781769aaf7b34d5d6388e0f7f235ffc79a8b8d3d
SavostinIllia <savostin.illia@gmail.com>
2020-07-06 19:12:01 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Layout from './hoc/Layout/Layout'
 import Quiz from './containers/Quiz/Quiz';
 
 function App() {
-  console.log('DEv Branch works')
+
   return (
     <Layout>
       <Quiz />
