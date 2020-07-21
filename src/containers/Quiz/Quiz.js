e40a2248bc6f912eceb7c34c8121ae23ef467206
SavostinIllia <savostin.illia@gmail.com>
2020-07-21 21:36:33 +0200

    converting Quiz Component in to REDUX, creating actions + dispatches fro Quiz Component

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,17 +4,10 @@ import ActiveQiuz from "../../components/ActiveQuiz/ActiveQuiz";
 import FinishedQuiz from "../../components/FinishedQuiz/FinishedQuiz";
 import axios from "../../axios/axios-quiz";
 import Loader from "../../components/UI/Loader/Loader";
+import { connect } from "react-redux";
+import { fetchQuizById } from "../../store/actions/quiz";
 
 class Quiz extends Component {
-  state = {
-    results: {},
-    isFinished: false,
-    activeQuestion: 0,
-    answerState: null,
-    quiz: [],
-    loading: true,
-  };
-
   onAnswerClickHandler = (answerID) => {
     if (this.state.answerState) {
       const key = Object.keys(this.state.answerState)[0];
@@ -69,20 +62,8 @@ class Quiz extends Component {
     });
   };
 
-  async componentDidMount() {
-    try {
-      const response = await axios.get(
-        `/quizes/${this.props.match.params.id}.json`
-      );
-      const quiz = response.data;
-
-      this.setState({
-        quiz,
-        loading: false,
-      });
-    } catch (e) {
-      console.log(e);
-    }
+  componentDidMount() {
+    this.props.fetchQuizById(this.props.match.params.id);
   }
 
   render() {
@@ -90,22 +71,22 @@ class Quiz extends Component {
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
           <h1>Please answer all questions</h1>
-          {this.state.loading ? (
+          {this.props.loading || !this.props.quiz ? (
             <Loader />
-          ) : this.state.isFinished ? (
+          ) : this.props.isFinished ? (
             <FinishedQuiz
-              results={this.state.results}
-              quiz={this.state.quiz}
+              results={this.props.results}
+              quiz={this.props.quiz}
               onRetry={this.retryHandler}
             />
           ) : (
             <ActiveQiuz
-              question={this.state.quiz[this.state.activeQuestion].question}
-              answers={this.state.quiz[this.state.activeQuestion].answers}
+              question={this.props.quiz[this.props.activeQuestion].question}
+              answers={this.props.quiz[this.props.activeQuestion].answers}
               onAnswerClick={this.onAnswerClickHandler}
-              quizLength={this.state.quiz.length}
-              answerNumber={this.state.activeQuestion + 1}
-              state={this.state.answerState}
+              quizLength={this.props.quiz.length}
+              answerNumber={this.props.activeQuestion + 1}
+              state={this.props.answerState}
             />
           )}
         </div>
@@ -114,4 +95,21 @@ class Quiz extends Component {
   }
 }
 
-export default Quiz;
+function mapStateToProps(state) {
+  return {
+    results: state.quiz.results,
+    isFinished: state.quiz.isFinished,
+    activeQuestion: state.quiz.activeQuestion,
+    answerState: state.quiz.answerState,
+    quiz: state.quiz.quiz,
+    loading: state.quiz.loading,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchQuizById: (id) => dispatch(fetchQuizById(id)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Quiz);
--- src/store/reducers/quiz.js
+++ src/store/reducers/quiz.js
@@ -2,11 +2,18 @@ import {
   FETCH_QUIZES_START,
   FETCH_QUIZES_SUCCESS,
   FETCH_QUIZES_ERROR,
+  FETCH_QUIZ_SUCCESS,
 } from "../actions/actionTypes";
 
 const initialState = {
   quizes: [],
   loading: false,
+  error: null,
+  results: {},
+  isFinished: false,
+  activeQuestion: 0,
+  answerState: null,
+  quiz: null,
 };
 
 export default function quizReducer(state = initialState, action) {
@@ -24,6 +31,8 @@ export default function quizReducer(state = initialState, action) {
       };
     case FETCH_QUIZES_ERROR:
       return { ...state, loading: false };
+    case FETCH_QUIZ_SUCCESS:
+      return { ...state, loading: false, quiz: action.quiz };
     default:
       return state;
   }
