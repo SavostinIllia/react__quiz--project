2d17c6d11989c29357634f90feb3164e7ad5041d
SavostinIllia <savostin.illia@gmail.com>
2020-07-23 15:39:02 +0200

    Depling Quiz-Project in firebase-host

---
+++ build/precache-manifest.86b239ca669a70fda7c9f860b9e57609.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ca7a22a924d8ade6b9eecd4cc7c577",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10a3d92e4a3d702adbd0",
+    "url": "/static/css/main.3095dcac.chunk.css"
+  },
+  {
+    "revision": "865704f07ab44640ba24",
+    "url": "/static/js/2.a734ea08.chunk.js"
+  },
+  {
+    "revision": "fae6e97322ddc0579046aa829b382727",
+    "url": "/static/js/2.a734ea08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a3d92e4a3d702adbd0",
+    "url": "/static/js/main.a668727e.chunk.js"
+  },
+  {
+    "revision": "79b303671f87825de984",
+    "url": "/static/js/runtime-main.e16c07f3.js"
+  }
+]);
